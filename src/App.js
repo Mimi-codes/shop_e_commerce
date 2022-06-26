c72ed8bea9639877c9f8f23d6acf5b50387c5e10
Tomi <mariamismael904@gmail.com>
2022-06-26 18:32:41 +0100

    ...

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import { Products, Navbar, Cart} from './components';
 import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import React from 'react';
+
+
+
 const App = () => {
   const [products, setProducts] = useState([])
   const [cart, setCart] = useState({})
@@ -18,9 +21,7 @@ const App = () => {
   }
 
   const fetchCart = async () => {
-    const cart = await commerce.cart.retrieve();
-  
-    setCart(cart)
+    setCart(await commerce.cart.retrieve());
   }
 
   const handleOnAddToCart = async (productId, quantity) => {
@@ -29,30 +30,54 @@ const item = await commerce.cart.add(productId, quantity);
 setCart(item.cart);
 } 
 
+const handleUpdateCartQty = async (productId, quantity) => {
+  const response = await commerce.cart.update(productId, { quantity });
+  setCart(response.cart)
+}
+
+const handleRemoveFromCart = async (productId) => {
+  const response = await commerce.cart.remove(productId)
+
+  setCart(response.cart);
+}
+
+const handleEmptyCart = async () => {
+  const response = await commerce.cart.empty();
+  setCart(response.cart)
+}
+
   useEffect(() => {
 fetchProducts();
 fetchCart();
   }, []);
-  
-  console.log(products)
+   
+  // console.log(products)
+  // console.log(cart)
   
   return (
-    <>
         <Router>
   <div>
     <Navbar totalItems = {cart.total_items} />
     <Switch>
       <Route exact path='/'>
-      <Products products = {products} onAddToCart = {handleOnAddToCart}/>
+      <Products products = {products} onAddToCart = {handleOnAddToCart} />
       </Route>
 
       <Route exact path='/cart'>
-      <Cart cart={cart} />
+      <Cart cart={cart} 
+      onUpdateCartQty = {handleUpdateCartQty}
+      onRemoveFromCart = {handleRemoveFromCart}    
+      onEmptyCart ={handleEmptyCart}
+      />
+      </Route>
+
+      <Route exact path='/checkout'>
+
       </Route>
     </Switch>
     </div>
-    </Router>
-    </>
+    </Router> 
+  
 
   );
 }
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,22 +2,28 @@ import React from 'react'
 import { Container, Typography, Button, Grid } from '@material-ui/core';
 import useStyles from './styles';
 import CartItem from './CartItem/CartItem';
+import { Link } from 'react-router-dom';
 
-const Cart = ({ cart }) => {
+const Cart = ({ cart,  onUpdateCartQty, onRemoveFromCart, onEmptyCart }) => {
    // it's the same as cart.line_items.length === 0
 
   const classes = useStyles();
 
 const EmptyCart = () => (
-    <Typography variant='subtitle1'>You have no items in your shopping cart, start adding some!</Typography>
+    <Typography variant='subtitle1'>You have no items in your shopping cart, start adding some,
+    <Link to='/' className={classes.link}>Start adding some</Link>!
+    </Typography>
 )
+
+
+if(!cart.line_items) return 'Loading..';
 
 const FilledCart = () => (
     <>
     <Grid container spacing={3}>
 {cart.line_items.map((item) => (
 <Grid item xs={12} sm={4} key={item.id}>
-<CartItem item={item} />
+<CartItem item={item} onUpdateCartQty = {onUpdateCartQty} onRemoveFromCart = {onRemoveFromCart} />
 </Grid>
 ))}
     </Grid>
@@ -27,8 +33,8 @@ const FilledCart = () => (
             Subtotal: {cart.subtotal.formatted_with_symbol}
         </Typography>
     <div> 
-    <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary" >Empty cart</Button>
-          <Button className={classes.checkoutButton} size="large" type="button" variant="contained" color="primary">Checkout</Button>
+    <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary" onClick={onEmptyCart} >Empty cart</Button>
+          <Button component={Link} to='/checkout' className={classes.checkoutButton} size="large" type="button" variant="contained" color="primary">Checkout</Button>
    </div>
     
     </div>
@@ -36,7 +42,6 @@ const FilledCart = () => (
     </>
 )
 
-if(!cart.line_items) return 'Loading..';
 
     return (
     <Container >
