6a845b7bb3dd27794d7fe2e721a8075d786b38e3
Tomi <mariamismael904@gmail.com>
2022-07-08 00:05:41 +0100

    ...

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -17,8 +17,8 @@ const Navbar = ({ totalItems }) => {
    <AppBar position='fixed' className={classes.appBar} color='inherit'>
     <Toolbar>
         <Typography component={Link} to='/' variant='h6' className={classes.title} color='inherit'>
-            <img src={logo} alt='My Wares' height='25px' className={classes.image} />
-        Commerce.js
+            <img src={logo} alt='commerce.js' height='25px' className={classes.image} />
+        My Wares
         </Typography>
 <div className={classes.grow} />
 {location.pathname === '/' && (
