c422bbed047b8aeb5f0b0f9ea0e36fd9880ef79a
Tomi <mariamismael904@gmail.com>
2022-07-08 00:10:08 +0100

    ...

---
+++ build/precache-manifest.0ffcaae9498ad83ef15763263af9c613.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6988b9fe619171f559f",
+    "url": "/static/css/main.253732d4.chunk.css"
+  },
+  {
+    "revision": "e6988b9fe619171f559f",
+    "url": "/static/js/main.e6988b9f.chunk.js"
+  },
+  {
+    "revision": "ee8408d5f64d712378a8",
+    "url": "/static/js/1.ee8408d5.chunk.js"
+  },
+  {
+    "revision": "1f03410eb9a94418a470",
+    "url": "/static/js/2.1f03410e.chunk.js"
+  },
+  {
+    "revision": "e83f273c929bdac0c329",
+    "url": "/static/js/runtime~main.e83f273c.js"
+  },
+  {
+    "revision": "e418b68926bd0c7cd76283df1904de0a",
+    "url": "/static/media/shopping.e418b689.jpg"
+  },
+  {
+    "revision": "a5181dc1dc095b72531855a03e6efe13",
+    "url": "/index.html"
+  }
+];
